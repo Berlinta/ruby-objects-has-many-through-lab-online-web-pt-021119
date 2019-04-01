b6475a3065ce57fe8e3cadfeab1290f47d194fb4
Nash Brancic <nashfft@gmail.com>
2019-04-01 11:21:08 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Song
+  
+  attr_accessor :date, :patient, :doctor
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+  
+  
+  
+end
